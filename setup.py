6636a15dcd5eae1e0f53cc6f49bcf68c39056c94
Vincent M. Le Corre <vincent.m.le.corre@gmail.com>
2023-09-21 11:15:22 +0200

    added require conf.py

--- setup.py
+++ setup.py
@@ -4,12 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="boar", # Replace with your own username
+    name="boar_pv", # Replace with your own username
     version="1.0.0",
     author="Vincent Le Corre, Larry Lueer",
     author_email="vincent.le.corre@fau.de",
     description="High throughput parameter extraction and experimental design with Bayesian optimization",
     long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='BSD 3-Clause License',
     url="https://github.com/i-MEET/boar",
     packages=setuptools.find_packages(),
     readme = "README.md",
@@ -17,7 +19,6 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "Intended Audience :: Science/Research",
-        "Status :: Beta",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
@@ -36,7 +37,18 @@ setuptools.setup(
         'sqlalchemy',
         'pyodbc',
         'openpyxl',
-        'futures'
+        'futures',
+        'torch',
+        'torchvision',
+        'torchaudio',
+        'ax-platform',
     ],
+    extras_require = {
+        'dev': [
+            'pytest',
+            'twine',
+        ],
+    },
+
         
 )
