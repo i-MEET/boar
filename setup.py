c6775e2f95f6df18d116f8ba5b1e9e4e321cedb0
Vincent M. Le Corre <vincent.m.le.corre@gmail.com>
2023-09-21 15:26:31 +0200

    test

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     license='BSD 3-Clause License',
     url="https://github.com/i-MEET/boar",
-    download_url="https://github.com/i-MEET/boar/archive/refs/tags/v1.0.1.tar.gz",
+    download_url="https://github.com/i-MEET/boar/archive/refs/tags/v1.0.0-beta.tar.gz",
     packages=setuptools.find_packages(),
     readme = "README.md",
     keywords=['Bayesian optimization', 'parameter extraction', 'experimental design', 'high throughput', 'solar cells'],
